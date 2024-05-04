4c5a7fb9f4d198130197cca6240ae988ff1a04d8
Eugenia Litvintsava <26181412mail.ru@gmail.com>
2024-05-04 14:54:08 +0300

    end work of css properties

--- react-gpt/src/components/Main/Main.jsx
+++ react-gpt/src/components/Main/Main.jsx
@@ -32,6 +32,18 @@ function Main() {
                     <img src= {assets.code_icon} alt="" />
                 </div>
             </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter your promt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">Powered by OpenAI</p>
+            </div>
         </div>
      </div>
   )
